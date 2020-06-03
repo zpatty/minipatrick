d2995d35e5edc276a0ee020ee634a049e6f00566
zpatty <zpatt720@gmail.com>
2020-06-03 15:18:51 -0400

    Asyncronous serial communication script for talking to the central microcontroller

---
+++ on_computer/serial_comms.py
@@ -0,0 +1,44 @@
+#!/home/zpatty/anaconda3/envs/cython-dev/bin/python
+
+# import async libraries
+import asyncio
+from asyncio import get_event_loop
+
+# async pyserial
+from serial_asyncio import open_serial_connection
+
+# library for async equivalent of input()
+from aioconsole import ainput
+
+
+async def main():
+    # open serial connection through the Streams API and get reader and writer objects
+    reader, writer = await open_serial_connection(url='/dev/ttyACM0', baudrate=115200)
+
+    # relay status to terminal
+    print("Opened port. Press reset on Central to initialize echoing. Ctrl-C to stop.")
+
+    # execute reader and writer coroutines concurrently
+    await asyncio.gather(read(reader),write(writer))
+
+# Reads from serial, converts all received messages to strings, and prints to terminal
+async def read(reader):
+    
+    while True:
+        line = await reader.readuntil(b'\n')
+        print(str(line, 'utf-8'))
+
+# Listen to terminal for user input and relay messages to serial
+async def write(writer):
+
+    # this wait is not functional, it is just to give the user time to reset Central
+    await asyncio.sleep(6) 
+
+    while True:
+        
+        msg = await ainput("Message to send over serial terminal: ")
+        msg = msg + '\n'
+        writer.write(msg.encode('UTF-8'))
+
+# run the program
+asyncio.run(main())

e0dc66b50e05a69cbe056806ba8f9742a707651f
zpatty <zpatt720@gmail.com>
2020-07-09 16:53:49 -0400

    added apriltag detection to CV to get automated orientation knowledge and draw a tracking bounding box. Still need to verify that orientation is correct.

--- on_computer/robot_cv_multiprocess.py
+++ on_computer/robot_cv_multiprocess.py
@@ -10,6 +10,7 @@ from pycpd import RigidRegistration
 from imutils.video import VideoStream
 from imutils.video import FPS
 import imutils
+from pupil_apriltags import Detector
 
 # update to get new location of bounding boxes
 def update_tracker(frame, trackers, fps):
@@ -60,40 +61,53 @@ def parse_tracker():
     return [args, OPENCV_OBJECT_TRACKERS]
 
 # Initialize tracking by specifying bounding boxes of ROI
-def init_tracking(frame, trackers, OPENCV_OBJECT_TRACKERS, args):
-    # select region with the robot
-    bounding_box = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
+def init_tracking(frame, trackers, april_center, OPENCV_OBJECT_TRACKERS, args):
+    # Try again if we don't get enough tracked points
+    num_boxes = 0
+    while num_boxes < 2:
+        # select region with the robot
+        # bounding_box = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
 
-    # crop frame to that region
-    cropped_frame = frame[int(bounding_box[1]):int(bounding_box[1]+bounding_box[3]), 
-        int(bounding_box[0]):int(bounding_box[0]+bounding_box[2])]
+        # # crop frame to that region
+        # cropped_frame = frame[int(bounding_box[1]):int(bounding_box[1]+bounding_box[3]), 
+        #     int(bounding_box[0]):int(bounding_box[0]+bounding_box[2])]
+        
+        pix = np.random.normal(150, 10, 2)
+        print(pix)
+        cropped_frame = frame[int(april_center[1]-pix[1]):int(april_center[1]+pix[1]), 
+            int(april_center[0]-pix[0]):int(april_center[0]+pix[0])]
+        # set number of features to find and create ORB detection object
+        n = 200
+        orb = cv2.ORB_create(nfeatures = n)
+        # find the keypoints with ORB
+        kp = orb.detect(cropped_frame,None)
+        # compute the descriptors with ORB
+        kp, des = orb.compute(cropped_frame, kp)
+        # convert keypoints to numpy
+        points2f = cv2.KeyPoint_convert(kp)
+        # set size of pixels
+        w = 75
+        h = w
+        # convert to bounding box array format
+        boxes = points2f - w
+        n = np.size(boxes, 0)
+        boxes = np.hstack((boxes, np.full((n,2), w)))
+        # consolidate overlapping rectangles (needs to be a tuple)
+        boxes = tuple(map(tuple,boxes))
+        boxes, weights = cv2.groupRectangles(boxes, 1, 0.5)
+        # recomute number of boxes
+        num_boxes = np.size(boxes, 0)
 
-    # set number of features to find and create ORB detection object
-    n = 200
-    orb = cv2.ORB_create(nfeatures = n)
-    # find the keypoints with ORB
-    kp = orb.detect(cropped_frame,None)
-    # compute the descriptors with ORB
-    kp, des = orb.compute(cropped_frame, kp)
-    # convert keypoints to numpy
-    points2f = cv2.KeyPoint_convert(kp)
-    # set size of pixels
-    w = 75
-    h = w
-    # convert to bounding box array format
-    boxes = points2f - w
-    n = np.size(boxes, 0)
-    boxes = np.hstack((boxes, np.full((n,2), w)))
-    # consolidate overlapping rectangles (needs to be a tuple)
-    boxes = tuple(map(tuple,boxes))
-    boxes, weights = cv2.groupRectangles(boxes, 1, 0.5)
-    # recomute number of boxes
-    n = np.size(boxes, 0)
-
-    # convert coordinates of boxes back to original frame coordinates
-    boxes[:,0:2] = boxes[:,0:2] + np.hstack((np.full((n,1), int(bounding_box[0])), 
-        np.full((n,1), int(bounding_box[1]))))
-
+        # convert coordinates of boxes back to original frame coordinates
+        # boxes[:,0:2] = boxes[:,0:2] + np.hstack((np.full((num_boxes,1), int(bounding_box[0])), 
+        #     np.full((num_boxes,1), int(bounding_box[1]))))
+        boxes[:,0:2] = boxes[:,0:2] + np.hstack((np.full((num_boxes,1), int(april_center[0]-pix[0])), 
+            np.full((num_boxes,1), int(april_center[1]-pix[1]))))
+        for box in boxes:
+            (x, y, w, h) = [int(v) for v in box]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(1)
     # add a tracker object for each box (needs to be a tuple)
     boxes = tuple(map(tuple,boxes))
     
@@ -112,59 +126,115 @@ def get_centerpoints(boxes):
     #print(centerpoints)
     return centerpoints.transpose()
 
+# get orientation between a reference point cloud and the current point cloud using pycpd module
+def get_orientation(new_centerpoints, first_centerpoints, april_pose):
+    reg = RigidRegistration(**{'X': new_centerpoints, 'Y': first_centerpoints})
+    reg.register()
+    s, R, t = reg.get_registration_parameters()
+    orientation = april_pose*R.transpose()
+    return orientation
+
+def get_april_pose(pipeline,cfg):
+
+    # Create streaming profile and extract camera intrinsics
+    profile = cfg.get_stream(rs.stream.color)
+    intr = profile.as_video_stream_profile().get_intrinsics()
+    camera_params = [intr.fx, intr.fy, intr.ppx, intr.ppy]
+
+    # Wait until detecting an apriltag before continuing on
+    detections = []
+    while detections == []:
+
+        frame = get_frame(pipeline)
+
+        # convert frame to grayscale for apriltag detection
+        grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # initialize and detect apriltag
+        detector = Detector(families='tagStandard41h12')
+        detections = detector.detect(grayscale_frame, estimate_tag_pose=True, 
+            camera_params = camera_params, tag_size = 0.022)
+
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(1)
+
+    # calculate apriltag pose
+    april_pose = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 0]])*np.array(detections[0].pose_R)
+    april_pose = april_pose[0:2,0:2]
+    april_center = detections[0].center
+    print("Detected!")
+
+    return [april_pose, april_center]
+
+
+def get_frame(pipeline):
+    # Wait for a coherent frame
+    frames = pipeline.wait_for_frames()
+    color_frame = frames.get_color_frame()
+
+    # Convert images to numpy arrays
+    frame = np.asanyarray(color_frame.get_data())
+    return frame
+
 # Function to execute CV and output to script
 def cv_process(queue):
 
-    # parse user specified tracking algorithm
+    # Parse user specified tracking algorithm
     args, OPENCV_OBJECT_TRACKERS = parse_tracker()
 
-    # initialize opencv multitracker
+    # Initialize opencv multitracker
     trackers = cv2.MultiTracker_create()
 
-    # initialize the bounding box coordinates of the object we are going
-    # to track
+    # Initialize the bounding box coordinates of the object we are going to track
     boxes = None
 
+    # Set up RealSense Stream
     pipeline = rs.pipeline()
     config = rs.config()
-    #config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
     # Start streaming
-    pipeline.start(config)
+    cfg = pipeline.start(config)
 
-    # initialize the FPS throughput estimator
+    # Find apriltag orientation
+    april_pose, april_center = get_april_pose(pipeline, cfg)
+    
+    # Initialize the FPS throughput estimator
     fps = None
-    while True:
-        # Can't put this stuff in a function bc pipeline needs to be 
-        # called where it was started
-        # Wait for a coherent frame
-        frames = pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
-        if not color_frame:
-            continue
-        # Convert images to numpy arrays
-        frame = np.asanyarray(color_frame.get_data())
-        
-        # check if the tracking has been initialized
-        if boxes is not None:
-            #  = boxes
-            frame, boxes = update_tracker(frame, trackers, fps)
-            new_boxes = boxes
-            # last_centerpoints = get_centerpoints(last_boxes)
-            new_centerpoints = get_centerpoints(new_boxes)
-            reg.X = new_centerpoints
-            reg.register()
-            s, R, t = reg.get_registration_parameters()
-            print(R)
 
-        # honestly not quite sure what this does
+    # Initialize output
+    state = []
+
+
+    # Main Loop
+    while True:
+
+        # Ask the camera for a frame
+        frame = get_frame(pipeline)
+        
+        # Check if the tracking has been initialized
+        if boxes is not None:
+
+            previous_boxes = boxes
+            previous_centerpoints = new_centerpoints
+            frame, boxes = update_tracker(frame, trackers, fps)
+            this_time = time.time()
+            new_boxes = boxes
+            # Get new orientation
+            new_centerpoints = get_centerpoints(new_boxes)
+            orientation = get_orientation(new_centerpoints, first_centerpoints, april_pose)
+            
+            # Collect state variables for output
+            state = new_centerpoints, previous_centerpoints, this_time, start_time, orientation, april_pose
+            
         if not queue.empty():
             try:
                 queue.get_nowait()
             except Empty:
                 pass
-        queue.put(frame)
+
+        # Put state variables in queue
+        queue.put(state)
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
         # if the 's' key is selected, we are going to "select" a bounding
@@ -172,12 +242,14 @@ def cv_process(queue):
 
 
         if key == ord("s"):
-            boxes = init_tracking(frame, trackers, OPENCV_OBJECT_TRACKERS, args)
+            boxes = init_tracking(frame, trackers, april_center, OPENCV_OBJECT_TRACKERS, args)
             first_boxes = boxes
-            first_centerpoints = get_centerpoints(first_boxes)
-            print(first_centerpoints)
-            reg = RigidRegistration(**{'X': first_centerpoints, 'Y': first_centerpoints})
+            new_centerpoints = get_centerpoints(first_boxes)
+            first_centerpoints = new_centerpoints
+            orientation = get_orientation(new_centerpoints, first_centerpoints, april_pose)
+            
             fps = FPS().start()
+            start_time = time.time()
 
         # if the `q` key was pressed, break from the loop
         elif key == ord("q"):
@@ -193,14 +265,11 @@ if __name__ == '__main__':
     cam_process = Process(target=cv_process, args=(queue,))
     cam_process.start()
     while True:
-        # get frame from the queue
-        frame = queue.get()
+        # get output from the queue
+        output = queue.get()
         # show the output frame
-        cv2.imshow("Copy",frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                cam_process.terminate()  # Don't do this if shared resources
-                break
+        if output != []:
+            print(output)
         time.sleep(0.5)
 
 
